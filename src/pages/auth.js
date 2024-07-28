38e4964170797577852368223c522a44c90c785c
Jeet Dekivadia <jeet.university@gmail.com>
2024-07-28 16:02:03 +0400

    Create auth.js

---
+++ src/pages/auth.js
@@ -0,0 +1,7 @@
+const AuthPage = () => (
+  <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <h1 className="text-3xl font-bold mb-6">Auth</h1>
+    <p className="text-xl">Please login or signup to access the pantry tracker.</p>
+  </div>
+)
+export default AuthPage
