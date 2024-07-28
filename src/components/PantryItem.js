980a01b6473265fb081f83960430a8d8b9e6bb11
Jeet Dekivadia <jeet.university@gmail.com>
2024-07-28 15:12:22 +0400

    Update PantryItem.js

--- src/components/PantryItem.js
+++ src/components/PantryItem.js
@@ -1,9 +1,15 @@
-// src/components/PantryItem.js
-const PantryItem = ({ item, deleteItem }) => (
-  <div>
-    <p>{item.name} - {item.quantity}</p>
-    <button onClick={() => deleteItem(item.id)}>Delete</button>
-  </div>
-);
+const PantryItem = ({ item, onDelete }) => {
+  return (
+    <div className="flex justify-between items-center bg-gray-100 p-4 m-2 rounded">
+      <div>
+        <p className="text-xl">{item.name}</p>
+        <p className="text-gray-600">{item.quantity}</p>
+      </div>
+      <button onClick={() => onDelete(item.id)} className="bg-red-500 text-white p-2 rounded">
+        Delete
+      </button>
+    </div>
+  );
+};
 
 export default PantryItem;
