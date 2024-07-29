5d6f1695f05e89dd717c1bf8f795629fed058fe3
Jeet Dekivadia <jeet.university@gmail.com>
2024-07-29 15:46:01 +0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Pantry Tracker</h1>
+      <p>This is your starting point for building the pantry tracker application.</p>
+    </div>
+  );
+}
