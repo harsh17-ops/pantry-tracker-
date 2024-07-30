f0772ccbf2f8986424d0931d298854d3bd76b03c
Jeet Dekivadia <jeet.university@gmail.com>
2024-07-30 12:08:25 +0400

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth';
 import { getAnalytics, isSupported } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyBnb71cLp5FNoEaPy65bVg7UdOob7iidA8",
+  authDomain: "pantry-tracker-jeet.firebaseapp.com",
+  projectId: "pantry-tracker-jeet",
+  storageBucket: "pantry-tracker-jeet.appspot.com",
+  messagingSenderId: "890128743179",
+  appId: "1:890128743179:web:396548bc4a90f402e9400e",
+  measurementId: "G-748DMNZG2N"
 };
 
 if (!getApps().length) {
