72ce15d4e66ecca99046e0f388e10bff504ca5de
Jeet Dekivadia <jeet.university@gmail.com>
2024-07-30 11:29:48 +0400

    Create _app.tsx

---
+++ src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from '../styles/theme';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  React.useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement!.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
