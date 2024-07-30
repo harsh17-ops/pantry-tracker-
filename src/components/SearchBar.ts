89cc4ef75eeeb614c28cb445d490fa143d4b0f5f
Jeet Dekivadia <jeet.university@gmail.com>
2024-07-30 11:54:14 +0400

    Create SearchBar.ts

---
+++ src/components/SearchBar.ts
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { TextField, InputAdornment } from '@mui/material';
+import { Search } from '@mui/icons-material';
+import { styled } from '@mui/system';
+
+const StyledTextField = styled(TextField)(({ theme }) => ({
+  margin: theme.spacing(1),
+  width: '100%',
+}));
+
+interface SearchBarProps {
+  onSearch: (query: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+  const [query, setQuery] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+    onSearch(event.target.value);
+  };
+
+  return (
+    <StyledTextField
+      label="Search pantry items"
+      variant="outlined"
+      value={query}
+      onChange={handleChange}
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <Search />
+          </InputAdornment>
+        ),
+      }}
+    />
+  );
+};
+
+export default SearchBar;
