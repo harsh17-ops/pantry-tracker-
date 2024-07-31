14b736d4ba26a60323d96d3388a01593192a41cb
Jeet Dekivadia <jeet.university@gmail.com>
2024-07-31 11:14:28 +0400

    Create SearchBar.tsx

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { TextField, InputAdornment } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+
+interface SearchBarProps {
+  onSearch: (query: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const query = event.target.value;
+    setSearchQuery(query);
+    onSearch(query);
+  };
+
+  return (
+    <TextField
+      fullWidth
+      variant="outlined"
+      placeholder="Search pantry items..."
+      value={searchQuery}
+      onChange={handleSearchChange}
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <SearchIcon />
+          </InputAdornment>
+        ),
+      }}
+    />
+  );
+};
+
+export default SearchBar;
