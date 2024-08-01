63bbec63c41247f3cf72dafb14f296b3a99c0402
Jeet Dekivadia <jeet.university@gmail.com>
2024-08-01 15:09:50 +0400

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,6 +13,10 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Pacifico&display=swap"
+          />
         </Head>
         <body>
           <Main />
